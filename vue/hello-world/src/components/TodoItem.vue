92281f7aa21ff830cff8129c3e97caba74804be1
hurui <hurui1987@gmail.com>
2021-05-06 10:08:45 +0800

    vue commit

---
+++ vue/hello-world/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="text">{{ todo.text }}</li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    todo: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.text {
+  color: #42b983 
+}
+</style>
